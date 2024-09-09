ec11abd84f3525d59332e714898319562d2e0c3e
Michael <email@>
2024-09-09 16:36:05 -0500

    pushed original camel.py file

---
+++ camel.py
@@ -0,0 +1,19 @@
+def camelcase(sentence) :
+    """Convert sentence to camelCase"""
+    title_case = sentence.title() # upper first letter of each word
+    upper_camel_cased = title_case.replace(' ', '') # remove spaces
+    # lowercase first letter, join with rest of string
+    # slices dont produce index out of bounds errors
+    # so this still works on empty strings, strings of length 1
+
+    return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
+
+def main():
+
+    sentence = input('Enter your sentence: ')
+    output = camelcase(sentence)
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
